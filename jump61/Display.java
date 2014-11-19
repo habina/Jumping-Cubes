3560da524dfd7874326643d16d5117d11792ff44
Dasheng Chen <habinacds@gmail.com>
2014-11-18 22:10:23 -0800

    add buttons and label for UI.

--- jump61/Display.java
+++ jump61/Display.java
@@ -29,7 +29,10 @@ class Display extends TopLevel implements Observer {
         _board = game.getBoard();
         _commandOut = new PrintWriter(commandWriter);
         _boardWidget = new BoardWidget(game, _commandOut);
-        add(_boardWidget, new LayoutSpec("y", 1, "width", 2));
+        addLabel("", "Winner", new LayoutSpec("y", 1));
+        addButton("New", "newGame", new LayoutSpec("y", 1));
+        addButton("Quit", "quit", new LayoutSpec("y", 1));
+        add(_boardWidget, new LayoutSpec("y", 0, "width", 3));
         addMenuButton("Game->New Game", "newGame");
         addMenuButton("Game->Auto Game", "auto");
         addMenuButton("Game->Quit", "quit");
@@ -44,6 +47,17 @@ class Display extends TopLevel implements Observer {
         _board.addObserver(this);
         _game.addObserver(this);
         display(true);
+    }
+
+    /** Display what action AI takes.
+     *  @param s winner to announce
+     */
+    public void setWinner(String s) {
+        if (s.equals("")) {
+            setLabel("Winner", String.format(""));
+        } else {
+            setLabel("Winner", String.format("%s wins.", s));
+        }
     }
 
     /** Response to "pkMode" button click. */
@@ -70,35 +84,47 @@ class Display extends TopLevel implements Observer {
     /** Response to "three" button click. */
     void three(String dummy) {
         _commandOut.println("size 3");
+        newGame(dummy);
     }
 
     /** Response to "four" button click. */
     void four(String dummy) {
         _commandOut.println("size 4");
+        newGame(dummy);
     }
 
     /** Response to "five" button click. */
     void five(String dummy) {
         _commandOut.println("size 5");
+        newGame(dummy);
     }
 
     /** Response to "six" button click. */
     void six(String dummy) {
         _commandOut.println("size 6");
+        newGame(dummy);
     }
 
     /** Response to "ten" button click. */
     void seven(String dummy) {
         _commandOut.println("size 7");
+        newGame(dummy);
     }
 
     /** Response to "Auto" button click. */
     void newGame(String dummy) {
+        if (autoMove) {
+            autoMove = false;
+            _commandOut.println("manual red");
+            _commandOut.println("auto blue");
+        }
+        setWinner("");
         _commandOut.println("new");
     }
 
     /** Response to "Auto" button click. */
     void auto(String dummy) {
+        autoMove = true;
         _commandOut.println("auto red");
         _commandOut.println("auto blue");
         _commandOut.println("start");
@@ -112,10 +138,16 @@ class Display extends TopLevel implements Observer {
     @Override
     public void update(Observable obs, Object obj) {
         _boardWidget.update();
+        Side s = _board.getWinner();
+        if (s != null) {
+            setWinner(s.toCapitalizedString());
+        }
         frame.pack();
         _boardWidget.repaint();
     }
 
+    /** True if auto mode is on. */
+    private boolean autoMove = false;
     /** The current game that I am controlling. */
     private Game _game;
     /** The board maintained by _game (readonly). */
